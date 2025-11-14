04c6056178e0811637777945979c9ab28ecf485c
Andrew Smirnov <smirnov@informatics.ru>
2025-11-14 22:36:37 +0300

    Add host env var

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 ADMIN_ID = os.getenv("ADMIN_ID")
 POLICY_URL = os.getenv("POLICY_URL")
+HOST = os.getenv('WEBHOOK_BOT_HOST')
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
