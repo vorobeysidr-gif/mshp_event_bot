e700299578723cba4ad76c01ff842364ee7cc7fc
Andrew Smirnov <srx64@yandex.ru>
2025-11-15 11:01:22 +0300

    Update config

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-# python-dotenv is optional for local testing; if it's not installed,
-# we silently continue and rely on the environment variables already set.
 from dotenv import load_dotenv
-
 import os
 
 load_dotenv()
@@ -10,5 +7,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 ADMIN_ID = os.getenv("ADMIN_ID")
 POLICY_URL = os.getenv("POLICY_URL")
-
+WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')
+WEBHOOK_PATH = os.getenv('WEBHOOK_PATH')
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
